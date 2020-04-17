e92ea36d59dd626cbebc731ac74c51b1e63becdd
khaladin <hobancor@gmail.com>
2020-04-16 21:40:56 -0400

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      In here we are getting data about gentrification, some basic data needed is the state code
+      florida is 12
+      <ul>
+        <li>florida state code: 12</li>
+        <li>example query https://api.census.gov/data/2018/acs/acsse?get=NAME,K200001_001E&for=state:12&key=YOUR_KEY_HERE</li>
+      </ul>
     </div>
   );
 }
